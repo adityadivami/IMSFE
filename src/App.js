296abb05f3fe918ce3cf3eaa1fdffd0ec4b27f96
aditya <aditya@divami.com>
2022-05-28 01:08:28 +0530

    added routers

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import AdminPage from "./pages/landingpages/admin/admin.page";
 import HeadPage from "./pages/landingpages/head/head.page";
 import JobSeekerPage from "./pages/landingpages/jobseeker/jobseeker.page";
 import Button from "@mui/material/Button";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      {/* <LoginPage /> */}
-      <AdminPage />
-      {/* <HeadPage />
-      <JobSeekerPage />
-      <Button variant="contained">Hello World</Button>  */}
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="admin" element={<AdminPage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="head" element={<HeadPage />} />
+        <Route path="candidate" element={<JobSeekerPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
